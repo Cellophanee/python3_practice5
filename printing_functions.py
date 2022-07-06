54c52f98ed11651050cf4493388241ea95557441
Cellophanee <102979762+Cellophanee@users.noreply.github.com>
2022-07-06 15:35:41 +0300

    Add files via upload

---
+++ printing_functions.py
@@ -0,0 +1,5 @@
+import models
+unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+models.print_models(unprinted_designs, completed_models)
+models.show_completed_models(completed_models)
